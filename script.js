9ccfb900444a364548d7d6160b4d86b469dc24e4
tw3ax <golovin.po@yandex.ru>
2025-02-25 21:57:22 +0300

    Revert "bug fixes"
    This reverts commit ecc217e3ec0d6809fa42fadee5060abefa4da3ac.

--- script.js
+++ script.js
@@ -24,7 +24,6 @@ function handleSearch(event) { // пасхалко-поисковик
 }
 // *партиклы
 function pop (e) {
-    if(e.target.dataset.type){
     let amount = 10;
     switch (e.target.dataset.type) {
         case 'shadow':
@@ -44,7 +43,6 @@ function pop (e) {
             createParticle(e.clientX, e.clientY, e.target.dataset.type);
         }
     }
-}
 }
 function createParticle (x, y, type) {
     const particle = document.createElement('particle');
